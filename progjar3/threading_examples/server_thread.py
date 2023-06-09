ac383ccbf4bd27d1c47d80207cf9264275fdad76
Muhammad Yunus <Yunusenvire@gmail.com>
2023-06-10 03:30:56 +0700

    feat: tugas-2 server_thread.py

--- progjar3/threading_examples/server_thread.py
+++ progjar3/threading_examples/server_thread.py
@@ -15,7 +15,19 @@ class ProcessTheClient(threading.Thread):
 		while True:
 			data = self.connection.recv(32)
 			if data:
-				self.connection.sendall(data)
+				# Ubah data menjadi string
+				data = data.decode('utf-8')
+				# Periksa apakah data memenuhi ketentuan
+				if data.startswith('TIME') and data.endswith('\r\n'):
+					current_time = time.strftime('%H:%M:%S', time.localtime())
+					response = f"JAM {current_time}\r\n"
+
+					# Kirim respon ke client
+					self.connection.sendall(response.encode('utf-8'))
+				else:
+					# Kirim pesan error jika data tidak sesuai format
+					error_message = "ERROR: Invalid request format\r\n"
+					self.connection.sendall(error_message.encode('utf-8'))
 			else:
 				break
 		self.connection.close()
@@ -27,8 +39,9 @@ class Server(threading.Thread):
 		threading.Thread.__init__(self)
 
 	def run(self):
-		self.my_socket.bind(('0.0.0.0',8889))
+		self.my_socket.bind(('0.0.0.0', 45000)) # Mengubah port menjadi 45000
 		self.my_socket.listen(1)
+		logging.warning(f"waiting for connection on port 45000") # Menunggu koneksi.
 		while True:
 			self.connection, self.client_address = self.my_socket.accept()
 			logging.warning(f"connection from {self.client_address}")
